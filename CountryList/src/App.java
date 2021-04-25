3e05373ba90d5bf10bf751b610e84f8c3f5ef2ef
u100247 <u100247@MM0012B7.local>
2021-04-25 08:04:07 +0200

    Lab 1 finished

--- CountryList/src/App.java
+++ CountryList/src/App.java
@@ -1,19 +1,32 @@
 package src;
+import java.util.LinkedList; //get from java package LinkedList method 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        List list = new List();
-        list.addFirst("Spain");
+        List list = new List (); // instance a list of countries
+        list.addFirst("Spain"); // add in the first position of the list
         list.addFirst("Andorra");
-        list.addLast("Holland");
+        list.addLast("Holland"); // add in the last position of the list
         list.addFirst("France");
-        //list.countList();
-        //list.printCountry();
-        //list.validateCountry();
-        //list.printList();
-        //list.extractPosition(0);
-        list.printList();
-        list.extractCountry("Andorra");
-        list.printList();
-    }
+        list.printList(); // print 
+        list.countList(); // count list
+        list.printCountry(); // print a country by position
+        list.validateCountry(); // validate a country by name
+        list.extractPosition(3); // extract a country by position
+        list.extractCountry("Andorra"); // extract a country by name
+        list.printList(); // print
+        LinkedList <String> linkedList1 = new LinkedList <String>();
+        linkedList1.addFirst("China");
+        linkedList1.addLast("Japan");
+        LinkedList <String> linkedList2 = new LinkedList <String>();
+        linkedList2.addLast("Korea");
+        linkedList2.addFirst("Taiwan");
+        LinkedList <String> concatList = new LinkedList <String> ();
+        concatList.addAll(linkedList1); // method of linkedList to concat 
+        concatList.addAll(linkedList2);
+        System.out.println("New linked list: " + concatList + "\n");
+        concatList.set(2, "Mexico"); // method of linkedList to replace an element by position
+        System.out.println("New linked list: " + concatList + "\n");
+             }
 }
+
--- CountryList/src/List.java
+++ CountryList/src/List.java
@@ -3,15 +3,15 @@ package src;
 import javax.swing.JOptionPane;
 
 public class List {
-    private Node first;
-    private Node last;
-    int size = 0;
-    int position = 0;
-    int n;
+    private Node first; // create at Node first element variable
+    private Node last; // create at Node last element variable
+    int size = 0; // variable for set list size
+    int position = 0; // variable for set element position
+    int n; // variable for iterator
 
-    public void addLast(String countryName) {
+    public void addLast(String countryName) { // method to add in last position an element
         Node node = new Node(countryName);
-        if (last == null && first == null) {
+        if (last == null && first == null) { // in case is empty list
             last = node;
             first = node;
         } else {
@@ -21,9 +21,9 @@ public class List {
         }
     }
  
-    public void addFirst(String countryName) {
+    public void addFirst(String countryName) { // method to add in last position an element
         Node node = new Node(countryName);
-        if (last == null && first == null) {
+        if (last == null && first == null) { // in case is empty list
             last = node;
             first = node;
         } else {
@@ -33,17 +33,19 @@ public class List {
         }
     }
 
-    public void printList(){
+    public void printList(){ //method to iterate in the list and print it
         for (Node i = first; i != null; i = i.getNextElement()) {
             System.out.printf("\n %s ", i.showList());
-            size++;
+            size++; //count size
         }
         System.out.println("\n");
     }
+
     public void countList(){
-        System.out.printf(" Size of the list is: %d \n", size);
+        System.out.printf(" Size of the list is: %d \n", size); // print size
     }
-    public void printCountry(){
+
+    public void printCountry(){ //method to print country in required position
         String texto =JOptionPane.showInputDialog("What position of the list do you want to print?");
         int n = Integer.parseInt(texto); //text to integer
         for (Node i = first; i != null; i = i.getNextElement()) {
@@ -54,7 +56,8 @@ public class List {
         }
         System.out.println("\n");
     }
-    public void validateCountry(){
+
+    public void validateCountry(){ // method to validate if a country is on the list
         String texto =JOptionPane.showInputDialog("What country do you want to validate?");
         for (Node i = first; i != null; i = i.getNextElement()) {
             String countryName = i.showList();
@@ -64,7 +67,8 @@ public class List {
         }
         System.out.println();
     }
-    public Object extractPosition(int n) {
+
+    public Object extractPosition(int n) { //method to extract a country by position
         Object out = null;
         Node iterator = first;
 
@@ -77,12 +81,13 @@ public class List {
             iterator.setNextElement(iterator.getNextElement().getNextElement());
         return out;
     }
-    public Object extractCountry(String countryName) {
+
+    public Object extractCountry(String countryName) { //method to extract a country by name
         Object out = null;
         for (Node i = first; i != null; i = i.getNextElement()) {
             String country = i.showList();
             if(country.equals(countryName)){
-                Node n = i.getPreElement();
+                Node n = i.getPreElement(); // get the previous country to set the new list
                 n.setNextElement(n.getNextElement().getNextElement());
             }
         }
